6d2b720f8d01eae0ad8ecf874a895a5f3deeddc7
gan-ta <gilmat@naver.com>
2025-04-06 02:22:38 +0900

    style: 인텔리제이 코드 스타일 수정

--- src/main/java/nextstep/qna/domain/Question.java
+++ src/main/java/nextstep/qna/domain/Question.java
@@ -10,112 +10,112 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Question {
-    private Long id;
+  private Long id;
 
-    private String title;
+  private String title;
 
-    private String contents;
+  private String contents;
 
-    private NsUser writer;
+  private NsUser writer;
 
-    private List<Answer> answers = new ArrayList<>();
+  private List<Answer> answers = new ArrayList<>();
 
-    private boolean deleted = false;
+  private boolean deleted = false;
 
-    private LocalDateTime createdDate = LocalDateTime.now();
+  private LocalDateTime createdDate = LocalDateTime.now();
 
-    private LocalDateTime updatedDate;
+  private LocalDateTime updatedDate;
 
-    public Question() {
+  public Question() {
+  }
+
+  public Question(NsUser writer, String title, String contents) {
+    this(0L, writer, title, contents, new ArrayList<>(), false);
+  }
+
+  public Question(NsUser writer, String title, String contents, List<Answer> answers) {
+    this(0L, writer, title, contents, answers, false);
+  }
+
+  public Question(NsUser writer, String title, String contents, List<Answer> answers, boolean deleted) {
+    this(0L, writer, title, contents, answers, deleted);
+  }
+
+  public Question(Long id, NsUser writer, String title, String contents) {
+    this(id, writer, title, contents, new ArrayList<>(), false);
+  }
+
+  public Question(Long id, NsUser writer, String title, String contents, List<Answer> answers, boolean deleted) {
+    this.id = id;
+    this.writer = writer;
+    this.title = title;
+    this.contents = contents;
+    this.answers = answers;
+    this.deleted = deleted;
+  }
+
+  public void addAnswer(Answer answer) {
+    answer.toQuestion(this);
+    answers.add(answer);
+  }
+
+  public boolean isOwner(NsUser loginUser) {
+    return writer.equals(loginUser);
+  }
+
+  public boolean isAllAnswersWrittenByUser(NsUser loginUser) {
+    return answers.stream().allMatch(answer -> answer.isOwner(loginUser));
+  }
+
+  public boolean isDeleted() {
+    return deleted;
+  }
+
+  public void checkDeletableByUser(NsUser loginUser) throws CannotDeleteException {
+    if (!isOwner(loginUser)) {
+      throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
     }
-
-    public Question(NsUser writer, String title, String contents) {
-        this(0L, writer, title, contents, new ArrayList<>(), false);
+    if (!isAllAnswersWrittenByUser(loginUser)) {
+      throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
     }
+  }
 
-    public Question(NsUser writer, String title, String contents, List<Answer> answers) {
-        this(0L, writer, title, contents, answers, false);
-    }
+  public void deleteWithAllAnswers() {
+    this.deleted = true;
+    answers.forEach(Answer::delete);
+  }
 
-    public Question(NsUser writer, String title, String contents,  List<Answer> answers, boolean deleted) {
-        this(0L, writer, title, contents, answers, deleted);
-    }
+  public List<DeleteHistory> toDeleteHistory() {
+    List<DeleteHistory> res = new ArrayList<>(
+        List.of(new DeleteHistory(ContentType.QUESTION, id, writer, LocalDateTime.now()))
+    );
 
-    public Question(Long id, NsUser writer, String title, String contents) {
-        this(id, writer, title, contents, new ArrayList<>(), false);
-    }
+    res.addAll(
+        answers.stream().map(Answer::toDeleteHistory).collect(Collectors.toList())
+    );
+    return res;
+  }
 
-    public Question(Long id, NsUser writer, String title, String contents, List<Answer> answers, boolean deleted) {
-        this.id = id;
-        this.writer = writer;
-        this.title = title;
-        this.contents = contents;
-        this.answers = answers;
-        this.deleted = deleted;
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Question question = (Question) o;
+    return deleted == question.deleted &&
+        Objects.equals(id, question.id) &&
+        Objects.equals(title, question.title) &&
+        Objects.equals(contents, question.contents) &&
+        Objects.equals(writer, question.writer) &&
+        Objects.equals(answers, question.answers);
+  }
 
-    public void addAnswer(Answer answer) {
-        answer.toQuestion(this);
-        answers.add(answer);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, title, contents, writer, answers, deleted, createdDate, updatedDate);
+  }
 
-    public boolean isOwner(NsUser loginUser) {
-        return writer.equals(loginUser);
-    }
-
-    public boolean isAllAnswersWrittenByUser(NsUser loginUser) {
-        return answers.stream().allMatch(answer -> answer.isOwner(loginUser));
-    }
-
-    public boolean isDeleted() {
-        return deleted;
-    }
-
-    public void checkDeletableByUser(NsUser loginUser) throws CannotDeleteException {
-        if (!isOwner(loginUser)) {
-            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
-        }
-        if (!isAllAnswersWrittenByUser(loginUser)) {
-            throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
-        }
-    }
-
-    public void deleteWithAllAnswers() {
-        this.deleted = true;
-        answers.forEach(Answer::delete);
-    }
-
-    public List<DeleteHistory> toDeleteHistory() {
-        List<DeleteHistory> res = new ArrayList<>(
-            List.of(new DeleteHistory(ContentType.QUESTION, id, writer, LocalDateTime.now()))
-        );
-
-        res.addAll(
-            answers.stream().map(Answer::toDeleteHistory).collect(Collectors.toList())
-        );
-        return res;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Question question = (Question) o;
-        return deleted == question.deleted &&
-            Objects.equals(id, question.id) &&
-            Objects.equals(title, question.title) &&
-            Objects.equals(contents, question.contents) &&
-            Objects.equals(writer, question.writer) &&
-            Objects.equals(answers, question.answers);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, title, contents, writer, answers, deleted, createdDate, updatedDate);
-    }
-
-    @Override
-    public String toString() {
-        return "Question [id=" + id + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
-    }
+  @Override
+  public String toString() {
+    return "Question [id=" + id + ", title=" + title + ", contents=" + contents + ", writer=" + writer + "]";
+  }
 }
--- src/main/java/nextstep/qna/service/QnAService.java
+++ src/main/java/nextstep/qna/service/QnAService.java
@@ -2,32 +2,31 @@ package nextstep.qna.service;
 
 import nextstep.qna.CannotDeleteException;
 import nextstep.qna.NotFoundException;
-import nextstep.qna.domain.*;
+import nextstep.qna.domain.AnswerRepository;
+import nextstep.qna.domain.Question;
+import nextstep.qna.domain.QuestionRepository;
 import nextstep.users.domain.NsUser;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service("qnaService")
 public class QnAService {
-    @Resource(name = "questionRepository")
-    private QuestionRepository questionRepository;
+  @Resource(name = "questionRepository")
+  private QuestionRepository questionRepository;
 
-    @Resource(name = "answerRepository")
-    private AnswerRepository answerRepository;
+  @Resource(name = "answerRepository")
+  private AnswerRepository answerRepository;
 
-    @Resource(name = "deleteHistoryService")
-    private DeleteHistoryService deleteHistoryService;
+  @Resource(name = "deleteHistoryService")
+  private DeleteHistoryService deleteHistoryService;
 
-    @Transactional
-    public void deleteQuestion(NsUser loginUser, long questionId) throws CannotDeleteException {
-        Question question = questionRepository.findById(questionId).orElseThrow(NotFoundException::new);
-        question.checkDeletableByUser(loginUser);
-        question.deleteWithAllAnswers();
-        deleteHistoryService.saveAll(question.toDeleteHistory());
-    }
+  @Transactional
+  public void deleteQuestion(NsUser loginUser, long questionId) throws CannotDeleteException {
+    Question question = questionRepository.findById(questionId).orElseThrow(NotFoundException::new);
+    question.checkDeletableByUser(loginUser);
+    question.deleteWithAllAnswers();
+    deleteHistoryService.saveAll(question.toDeleteHistory());
+  }
 }
