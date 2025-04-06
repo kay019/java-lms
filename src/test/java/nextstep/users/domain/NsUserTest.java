f8a6a3f8a1f119c649065a8a43379241b8545a25
selene-p <hwp0223@gmail.com>
2025-04-06 18:30:27 +0900

    test: NsUser 테스트코드 작성

--- src/test/java/nextstep/users/domain/NsUserTest.java
+++ src/test/java/nextstep/users/domain/NsUserTest.java
@@ -1,6 +1,36 @@
 package nextstep.users.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class NsUserTest {
-    public static final NsUser JAVAJIGI = new NsUser(1L, "javajigi", "password", "name", "javajigi@slipp.net");
-    public static final NsUser SANJIGI = new NsUser(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+
+    private NsUser user;
+    private NsUser guestUser;
+
+    @BeforeEach
+    public void setUp() {
+        user = new NsUser(1L, "userId", "password", "name", "email");
+        guestUser = NsUser.GUEST_USER;
+    }
+
+    @Test
+    public void 일반_사용자_확인() {
+        assertThat(user.isGuestUser()).isFalse();
+    }
+
+    @Test
+    public void 게스트_사용자_확인() {
+        assertThat(guestUser.isGuestUser()).isTrue();
+    }
+
+    @Test
+    public void NsUser_생성_및_확인() {
+        NsUser newUser = new NsUser(2L, "newUserId", "newPassword", "newName", "newEmail");
+
+        assertThat(newUser.toString()).contains("newUserId", "newName", "newEmail");
+        assertThat(newUser.isGuestUser()).isFalse();
+    }
 }
