278de1df2962b01a8b90a49bd5ee2e7e4a5147eb
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-04-07 22:08:29 +0900

    [LMS] step1: Question

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,8 +1,32 @@
 package nextstep.qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
 
 public class QuestionTest {
     public static final Question Q1 = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
+
+    @Test
+    @DisplayName("질문 삭제 성공")
+    public void delete_성공() throws CannotDeleteException {
+        DeleteHistory deleteHistory = Q1.delete(NsUserTest.JAVAJIGI);
+        assertThat(Q1.isDeleted()).isTrue();
+        assertThat(deleteHistory).isEqualTo(new DeleteHistory(ContentType.QUESTION, Q1.getId(), Q1.getWriter(), LocalDateTime.now()));
+    }
+
+    @Test
+    @DisplayName("질문 삭제 실패")
+    public void delete_실패() throws CannotDeleteException {
+        assertThatThrownBy(() -> Q2.delete(NsUserTest.JAVAJIGI))
+            .isInstanceOf(CannotDeleteException.class)
+            .hasMessage("질문을 삭제할 권한이 없습니다.");
+    }
 }
