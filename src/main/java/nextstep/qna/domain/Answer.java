e611eaa938296ccb719ac08f6657020237d1864e
yang Jiyoung <0112jyoung@naver.com>
2025-04-08 21:57:34 +0900

    [refactor] 미사용 함수 제거

--- src/main/java/nextstep/qna/domain/Answer.java
+++ src/main/java/nextstep/qna/domain/Answer.java
@@ -17,7 +17,7 @@ public class Answer {
 
     private boolean deleted = false;
 
-    private LocalDateTime createdDate = LocalDateTime.now();
+    private final LocalDateTime createdDate = LocalDateTime.now();
 
     private LocalDateTime updatedDate;
 
@@ -47,11 +47,6 @@ public class Answer {
         return id;
     }
 
-    public Answer setDeleted(boolean deleted) {
-        this.deleted = deleted;
-        return this;
-    }
-
     public boolean isDeleted() {
         return deleted;
     }
@@ -62,10 +57,6 @@ public class Answer {
 
     public NsUser getWriter() {
         return writer;
-    }
-
-    public String getContents() {
-        return contents;
     }
 
     public void toQuestion(Question question) {
