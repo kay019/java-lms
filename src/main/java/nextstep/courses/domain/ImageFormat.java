c6a143804e5be1def810592e531ca3620a2d2e0b
luka.redwing(이재석)/kakao <luka.redwing@kakaocorp.com>
2025-04-11 16:59:05 +0900

    feat 이미지 포맷 구현

---
+++ src/main/java/nextstep/courses/domain/ImageFormat.java
@@ -0,0 +1,29 @@
+package nextstep.courses.domain;
+
+import static java.util.Collections.emptySet;
+
+import java.util.Set;
+
+public enum ImageFormat {
+    JPEG(Set.of("jpg", "jpeg")),
+    PNG(Set.of("png")),
+    GIF(Set.of("gif")),
+    SVG(Set.of("svg")),
+    NOT_SUPPORTED(emptySet());
+
+    private final Set<String> allFormat;
+
+    ImageFormat(Set<String> allFormat) {
+        this.allFormat = allFormat;
+    }
+
+    public static ImageFormat findFormat(String format) {
+        for (ImageFormat imageFormat : values()) {
+            if (imageFormat.allFormat.contains(format.toLowerCase())) {
+                return imageFormat;
+            }
+        }
+        return NOT_SUPPORTED;
+    }
+
+}
