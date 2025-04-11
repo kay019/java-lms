b893db82485b04704dcca7a38aba791cdd88082e
luka.redwing(이재석)/kakao <luka.redwing@kakaocorp.com>
2025-04-11 17:11:53 +0900

    feat 이미지 크기 제약사항 구현

---
+++ src/main/java/nextstep/courses/domain/ImageSize.java
@@ -0,0 +1,33 @@
+package nextstep.courses.domain;
+
+public class ImageSize {
+    private static final int MIN_WIDTH = 300;
+    private static final int MIN_HEIGHT = 200;
+    private static final double RATIO = 1.5;
+    private static final long MAX_SIZE = 1 * 1024 * 1024; // 1MB
+
+    private int width;
+    private int height;
+    private long size;
+
+    public ImageSize(int width, int height, long size) {
+        validSize(width, height, size);
+        this.width = width;
+        this.height = height;
+        this.size = size;
+    }
+
+    private void validSize(int width, int height, long size) {
+        if (width < MIN_WIDTH || height < MIN_HEIGHT) {
+            throw new IllegalArgumentException("이미지의 크기가 너무 작습니다.");
+        }
+
+        if ((double) width / height != RATIO) {
+            throw new IllegalArgumentException("이미지의 비율이 맞지 않습니다.");
+        }
+
+        if (size > MAX_SIZE) {
+            throw new IllegalArgumentException("이미지의 용량이 너무 큽니다.");
+        }
+    }
+}
---
+++ src/test/java/nextstep/courses/domain/ImageSizeTest.java
@@ -0,0 +1,49 @@
+package nextstep.courses.domain;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+class ImageSizeTest {
+
+    @Test
+    void 정상_이미지() {
+        assertThatNoException().isThrownBy(() ->
+                new ImageSize(300, 200, 1 * 1024 * 1024)  // 직접 값 전달
+        );
+    }
+
+    @Test
+    void 가로가_작은_이미지() {
+        assertThatThrownBy(() ->
+                new ImageSize(299, 200, 1 * 1024 * 1024)  // 직접 값 전달
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이미지의 크기가 너무 작습니다.");
+    }
+
+    @Test
+    void 세로가_작은_이미지() {
+        assertThatThrownBy(() ->
+                new ImageSize(300, 199, 1 * 1024 * 1024)  // 직접 값 전달
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이미지의 크기가 너무 작습니다.");
+    }
+
+    @Test
+    void 비율이_틀린_이미지() {
+        assertThatThrownBy(() ->
+                new ImageSize(300, 201, 1 * 1024 * 1024)  // 직접 값 전달
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이미지의 비율이 맞지 않습니다.");
+    }
+
+    @Test
+    void 너무_큰_이미지() {
+        assertThatThrownBy(() ->
+                new ImageSize(300, 200, 2 * 1024 * 1024)  // 직접 값 전달
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이미지의 용량이 너무 큽니다.");
+    }
+
+}
