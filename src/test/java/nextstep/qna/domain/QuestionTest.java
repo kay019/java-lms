e179e1bc37f3ae3b7d072f38fc02234d9563e75f
panya <kjy2844@kaist.ac.kr>
2025-04-11 16:32:13 +0900

    feat: 로그인 사용자와 질문한 사람이 같은 경우 삭제 가능하다.

--- src/test/java/nextstep/qna/domain/QuestionTest.java
+++ src/test/java/nextstep/qna/domain/QuestionTest.java
@@ -1,7 +1,9 @@
 package nextstep.qna.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import nextstep.qna.CannotDeleteException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,12 +14,23 @@ public class QuestionTest {
     public static final Question Q2 = new Question(NsUserTest.SANJIGI, "title2", "contents2");
 
     @Test
-    @DisplayName("삭제하면 삭제 상태로 변경된다.")
-    void testDelete() {
+    @DisplayName("내가 쓴 질문은 삭제할 수 있다.")
+    void testDelete() throws CannotDeleteException {
         Question question = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
 
         assertThat(question.isDeleted()).isFalse();
-        question.delete();
+        question.delete(NsUserTest.JAVAJIGI);
         assertThat(question.isDeleted()).isTrue();
     }
+
+    @Test
+    @DisplayName("다른 사람이 쓴 질문은 삭제할 수 없다.")
+    void testDeleteWithSameWriter() {
+        Question question = new Question(NsUserTest.JAVAJIGI, "title1", "contents1");
+
+        assertThatThrownBy(() -> {
+            question.delete(NsUserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
+
 }
