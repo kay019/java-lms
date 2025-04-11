7380959723ffd5afd8e3265046d519dcfcae18d9
panya <kjy2844@kaist.ac.kr>
2025-04-11 16:38:50 +0900

    feat: 답변을 삭제할 때 삭제 상태(deleted)를 변경한다.

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,22 @@
 package nextstep.qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("답변을 삭제할 때 삭제 상태(deleted)를 변경한다.")
+    void testDelete() {
+        Answer answer = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+
+        assertThat(answer.isDeleted()).isFalse();
+        answer.delete();
+        assertThat(answer.isDeleted()).isTrue();
+    }
 }
