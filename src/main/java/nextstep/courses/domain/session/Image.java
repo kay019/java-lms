4760b6851ea14454b11c081a03e7c910a08ff02a
JSL\jsl <luka756@naver.com>
2025-04-13 05:54:03 +0900

    refactor 이미지 유효성 검사 수정

--- src/main/java/nextstep/courses/domain/session/Image.java
+++ src/main/java/nextstep/courses/domain/session/Image.java
@@ -7,15 +7,19 @@ public class Image {
 
     public Image(String url, int width, int height, long size, String formatStr) {
         ImageFormat imageFormat = ImageFormat.findFormat(formatStr);
-        if (imageFormat == ImageFormat.NOT_SUPPORTED) {
-            throw new IllegalArgumentException("지원하지 않는 이미지 포맷입니다.");
-        }
+        validateCheck(imageFormat);
 
         ImageSize imageSize = new ImageSize(width, height, size);
 
         this.url= url;
         this.size = imageSize;
         this.format = imageFormat;
+    }
+
+    private void validateCheck(ImageFormat imageFormat){
+        if (imageFormat == ImageFormat.NOT_SUPPORTED) {
+            throw new IllegalArgumentException("지원하지 않는 이미지 포맷입니다.");
+        }
     }
 
 }
