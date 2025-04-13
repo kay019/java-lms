371d340b8903869be1b3a999069afbb8b0f6018f
selene-p <hwp0223@gmail.com>
2025-04-13 14:01:23 +0900

    feat: 강의 상태 추가

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -9,17 +9,20 @@ public class Session {
   private String title;
   private LocalDateTime startDate;
   private LocalDateTime endDate;
+  private SessionStatus status;
 
   public Session(Course course, String title, LocalDateTime startDate, LocalDateTime endDate) {
-    this(0L, course, title, startDate, endDate);
+    this(0L, course, title, startDate, endDate, SessionStatus.PREPARING);
   }
 
-  public Session(Long id, Course course, String title, LocalDateTime startDate, LocalDateTime endDate) {
+  public Session(Long id, Course course, String title, LocalDateTime startDate, LocalDateTime endDate, SessionStatus status) {
     this.id = id;
     this.course = course;
     this.title = title;
     this.startDate = startDate;
     this.endDate = endDate;
+    this.status = status;
   }
+
 
 }
