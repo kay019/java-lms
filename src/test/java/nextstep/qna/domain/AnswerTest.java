9ba1ebff6b3e779d5df5f320e53be12338097edc
domo7304 <domo7304@naver.com>
2025-04-13 15:02:51 +0900

    feat: Answer 클래스 내 답변 삭제 메서드 구현

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,45 @@
 package nextstep.qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 public class AnswerTest {
-    public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
-    public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    public Answer answer;
+
+    @BeforeEach
+    void setUp() {
+        answer = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
+    }
+
+    @Nested
+    @DisplayName("Answer는 현재 답변이 삭제 가능한 상태인지 아닌지 판단할 수 있다.")
+    class isSameUser {
+
+        @Test
+        @DisplayName("로그인한 사용자와 답변의 작성자가 같다면 DeleteHistory 객체를 반환한다.")
+        void loginQuestionIsSame() {
+            DeleteHistory history = answer.deleteBy(NsUserTest.JAVAJIGI);
+            assertThat(answer.isDeleted()).isTrue();
+            assertThat(history).isNotNull();
+            assertThat(history.getContentType()).isEqualTo(ContentType.ANSWER);
+            assertThat(history.getContentId()).isEqualTo(answer.getId());
+            assertThat(history.getDeletedBy()).isEqualTo(NsUserTest.JAVAJIGI);
+        }
+
+        @Test
+        @DisplayName("로그인한 사용자와 답변의 작성자가 다르다면 예외를 발생시킨다.")
+        void loginQuestionIsNotSame() {
+            assertThatThrownBy(() -> {
+                answer.deleteBy(NsUserTest.SANJIGI);
+            }).isInstanceOf(CannotDeleteException.class);
+        }
+    }
 }
