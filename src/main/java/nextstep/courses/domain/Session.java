567cf7b5eefa2895bfb791e6eca5e9de0aa0d629
Jiyoung Won <jiyoung928@gmail.com>
2025-04-16 15:42:58 +0900

    feature : Session 도메인 로직 구현 및 테스트 코드 추가

---
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -0,0 +1,63 @@
+package nextstep.courses.domain;
+
+import nextstep.payments.domain.Payment;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Session {
+    private final List<Long> enrolledUserIds = new ArrayList<>();// 준비중, 모집중, 종료
+    private Long id;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private CoverImage coverImaage;
+    private boolean free;
+    private long amount;
+    private int maxCapacity;
+    private SessionStatus status;
+
+    public Session(Long id, LocalDateTime startDate, LocalDateTime endDate, CoverImage coverImaage, boolean free, long amount, int maxCapacity, SessionStatus status) {
+        this.id = id;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.coverImaage = coverImaage;
+        this.free = free;
+        this.amount = amount;
+        this.maxCapacity = maxCapacity;
+        this.status = status;
+    }
+
+    public Session(Long id, CoverImage coverImaage, boolean free, long amount, int maxCapacity, SessionStatus status) {
+        this(id, LocalDateTime.now(), LocalDateTime.now(), coverImaage, free, amount, maxCapacity, status);
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getEnrolledUserCount() {
+        return enrolledUserIds.size();
+    }
+
+    public void enroll(Payment payment) {
+        if (!free) {
+            validPaidSessionEnroll(payment);
+        }
+        if (!this.status.equals(SessionStatus.모집중)) {
+            throw new IllegalArgumentException("강의 수강신청은 강의 상태가 모집중일 때만 가능합니다.");
+        }
+
+        enrolledUserIds.add(payment.getNsUserId());
+    }
+
+    private void validPaidSessionEnroll(Payment payment) {
+        if (getEnrolledUserCount() >= maxCapacity) {
+            throw new IllegalArgumentException("유료 강의는 강의 최대 수강 인원을 초과할 수 없습니다.");
+        }
+        if (this.amount != payment.getAmount()) {
+            throw new IllegalArgumentException("유료 강의는 수강생이 결제한 금액과 수강료가 일치할 때 수강 신청이 가능합니다.");
+        }
+    }
+}
---
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -0,0 +1,52 @@
+package nextstep.courses.domain;
+
+import nextstep.payments.domain.Payment;
+import nextstep.users.domain.NsUser;
+import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class SessionTest {
+
+    public static final CoverImage coverImage = new CoverImage("gif", 1024 * 1024, 300, 200);
+    public static final Session FREE_SESSION = new Session(1L, coverImage, true, 0, 1, SessionStatus.모집중);
+    public static final Session PAID_SESSION = new Session(2L, coverImage, false, 1000L, 1, SessionStatus.모집중);
+
+
+    @Test
+    void 무료_강의_정원_무제한() {
+        FREE_SESSION.enroll(new Payment("p1", NsUserTest.JAVAJIGI.getId(), FREE_SESSION.getId(), 0L));
+        FREE_SESSION.enroll(new Payment("p2", NsUserTest.SANJIGI.getId(), FREE_SESSION.getId(), 0L));
+        assertThat(FREE_SESSION.getEnrolledUserCount()).isEqualTo(2);
+    }
+
+    @Test
+    void 유료_강의_정원_초과_불가() {
+        PAID_SESSION.enroll(new Payment("p1", NsUserTest.JAVAJIGI.getId(), FREE_SESSION.getId(), 1000L));
+        assertThatThrownBy(() -> PAID_SESSION.enroll(new Payment("p2", NsUserTest.SANJIGI.getId(), PAID_SESSION.getId(), 1000L))).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 유료_강의_결제_금액_다름() {
+        assertThatThrownBy(() -> PAID_SESSION.enroll(new Payment("p1", NsUserTest.JAVAJIGI.getId(), PAID_SESSION.getId(), 1000L))).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 유료_강의_모집중_아님() {
+        assertThatThrownBy(() -> PAID_SESSION.enroll(new Payment("p1", NsUserTest.JAVAJIGI.getId(), PAID_SESSION.getId(), 1000L))).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 유료_강의_수강신청_성공() {
+        PAID_SESSION.enroll(new Payment("p1", NsUserTest.JAVAJIGI.getId(), PAID_SESSION.getId(), 1000L));
+        assertThat(PAID_SESSION.getEnrolledUserCount()).isEqualTo(1);
+
+    }
+
+
+}
