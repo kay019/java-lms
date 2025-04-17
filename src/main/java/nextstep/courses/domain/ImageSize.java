39533b5440fcd3320821469f71b54687b47e36db
jayden(김준기) <jayden.i@kakaocorp.com>
2025-04-17 20:18:15 +0900

    ImageSize 도메인을 구현한다.

--- src/main/java/nextstep/courses/domain/ImageSize.java
+++ src/main/java/nextstep/courses/domain/ImageSize.java
@@ -1,6 +1,40 @@
 package nextstep.courses.domain;
 
+import nextstep.exception.ImageSizeIllegalArgumentException;
+
 public class ImageSize {
-    private int width;
-    private int height;
+    private static final int MIN_WIDTH = 300;
+    private static final int MIN_HEIGHT = 200;
+
+    private final int width;
+    private final int height;
+
+    public ImageSize(int width, int height) {
+        validate(width, height);
+
+        this.width = width;
+        this.height = height;
+    }
+
+    private void validate(int width, int height) {
+        if (width < MIN_WIDTH || height < MIN_HEIGHT) {
+            throw new ImageSizeIllegalArgumentException();
+        }
+
+        if (!isValidAspectRatio(width, height)) {
+            throw new ImageSizeIllegalArgumentException();
+        }
+    }
+
+    private boolean isValidAspectRatio(int width, int height) {
+        return width * 2 == height * 3;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 }
