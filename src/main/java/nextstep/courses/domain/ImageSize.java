4eb0161b467677fb68848fac21229a94f8d77d89
jayden(김준기) <jayden.i@kakaocorp.com>
2025-04-18 18:09:19 +0900

    [리뷰 반영] ImageSize 값 객체 equals 추가

--- src/main/java/nextstep/courses/domain/ImageSize.java
+++ src/main/java/nextstep/courses/domain/ImageSize.java
@@ -1,5 +1,7 @@
 package nextstep.courses.domain;
 
+import java.util.Objects;
+
 import nextstep.exception.ImageSizeIllegalArgumentException;
 
 public class ImageSize {
@@ -36,5 +38,18 @@ public class ImageSize {
 
     public int getHeight() {
         return height;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ImageSize imageSize = (ImageSize) o;
+        return width == imageSize.width && height == imageSize.height;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(width, height);
     }
 }
