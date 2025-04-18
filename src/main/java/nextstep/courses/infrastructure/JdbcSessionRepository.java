a9a1f9f4e599881f18122ad2fbd45422efce38eb
gan-ta <gilmat@naver.com>
2025-04-18 10:42:23 +0900

    feat: SessionRepository 추가

--- src/main/java/nextstep/courses/infrastructure/JdbcSessionRepository.java
+++ src/main/java/nextstep/courses/infrastructure/JdbcSessionRepository.java
@@ -1,10 +1,15 @@
 package nextstep.courses.infrastructure;
 
-import nextstep.courses.domain.session.SessionRepository;
 import nextstep.courses.domain.session.Session;
+import nextstep.courses.domain.session.SessionRepository;
+import nextstep.courses.entity.SessionEntity;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository("sessionRepository")
@@ -16,7 +21,7 @@ public class JdbcSessionRepository implements SessionRepository {
     }
 
     @Override
-    public int save(Session session) {
+    public int save(Session session, Long courseId) {
         String sql = "insert into session (" +
             "created_at," +
             "deleted," +
@@ -34,7 +39,7 @@ public class JdbcSessionRepository implements SessionRepository {
             sql,
             session.createdAt(),
             session.deleted(),
-            session.courseId(),
+            courseId,
             session.fee(),
             session.capacity(),
             session.imageUrl(),
@@ -47,12 +52,47 @@ public class JdbcSessionRepository implements SessionRepository {
     }
 
     @Override
-    public Session findById(Long sessionId) {
-        return null;
+    public SessionEntity findById(Long id) {
+        String sessionSql = "select " +
+            "id," +
+            "created_at," +
+            "updated_at," +
+            "deleted," +
+            "course_id," +
+            "fee," +
+            "capacity," +
+            "image_url," +
+            "image_type," +
+            "start_date," +
+            "end_date," +
+            "type," +
+            "status " +
+            "from session where id = ?";
+
+        RowMapper<SessionEntity> rowMapper = (rs, rowNum) -> SessionEntity.builder()
+            .id(rs.getLong("id"))
+            .createdAt(toLocalDateTime(rs.getTimestamp("created_at")))
+            .updatedAt(toLocalDateTime(rs.getTimestamp("updated_at")))
+            .deleted(rs.getBoolean("deleted"))
+            .courseId(rs.getLong("course_id"))
+            .fee(rs.getLong("fee"))
+            .capacity(rs.getInt("capacity"))
+            .imageUrl(rs.getString("image_url"))
+            .imageType(rs.getString("image_type"))
+            .startDate(toLocalDateTime(rs.getTimestamp("start_date")))
+            .endDate(toLocalDateTime(rs.getTimestamp("end_date")))
+            .type(rs.getString("type"))
+            .status(rs.getString("status"))
+            .build();
+
+        return jdbcTemplate.queryForObject(sessionSql, rowMapper, id);
     }
 
-    @Override
-    public List<Session> findByCourse(Long courseId) {
-        return null;
+
+    private LocalDateTime toLocalDateTime(Timestamp timestamp) {
+        if (timestamp == null) {
+            return null;
+        }
+        return timestamp.toLocalDateTime();
     }
 }
--- src/test/java/nextstep/courses/infrastructure/JdbcSessionRepositoryTest.java
+++ src/test/java/nextstep/courses/infrastructure/JdbcSessionRepositoryTest.java
@@ -1,7 +1,5 @@
 package nextstep.courses.infrastructure;
 
-import nextstep.courses.domain.Course;
-import nextstep.courses.domain.CourseRepository;
 import nextstep.courses.domain.session.Session;
 import nextstep.courses.domain.session.SessionDescriptor;
 import nextstep.courses.domain.session.SessionPeriod;
@@ -13,12 +11,14 @@ import nextstep.courses.domain.session.image.SessionImageType;
 import nextstep.courses.domain.session.policy.SessionEnrollPolicy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -37,6 +37,7 @@ class JdbcSessionRepositoryTest {
 
     @DisplayName("강의 저장 테스트")
     @Test
+    @Order(1)
     void testSave() {
         ImageHandler imageHandlerStub = new ImageHandler() {
             @Override
@@ -54,7 +55,37 @@ class JdbcSessionRepositoryTest {
             }
         };
 
-        Course course = new Course(2L, "TDD, 클린 코드 with Java", 1L);
+        SessionConstraint constraint = new SessionConstraint(1000L, 50);
+
+        SessionDescriptor descriptor = new SessionDescriptor(
+            new SessionImage("http://test", imageHandlerStub, SessionImageType.JPEG),
+            new SessionPeriod(LocalDateTime.now(), LocalDateTime.now().plusDays(1)),
+            new SessionEnrollPolicy()
+        );
+
+        Session session = new Session(constraint, descriptor);
+        assertThat(sessionRepository.save(session, 1L)).isEqualTo(1);
+    }
+
+    @DisplayName("강의 조회 테스트")
+    @Test
+    @Order(2)
+    void testFindById() throws IOException {
+        ImageHandler imageHandlerStub = new ImageHandler() {
+            @Override
+            public BufferedImage image() {
+                return new BufferedImage(300, 200, BufferedImage.TYPE_INT_ARGB);
+            }
+
+            @Override
+            public void updateImage() {
+            }
+
+            @Override
+            public long byteSize() {
+                return 1024L * 866L;
+            }
+        };
 
         SessionConstraint constraint = new SessionConstraint(1000L, 50);
 
@@ -64,7 +95,8 @@ class JdbcSessionRepositoryTest {
             new SessionEnrollPolicy()
         );
 
-        Session session = new Session(course, constraint, descriptor);
-        assertThat(sessionRepository.save(session)).isEqualTo(1);
+        Session session = new Session(constraint, descriptor);
+        sessionRepository.save(session, 1L);
+        assertThat(sessionRepository.findById(2L)).isNotNull();
     }
 }
