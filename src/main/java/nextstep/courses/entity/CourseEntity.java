773117a365daed8c6da0836c2e52288135e11b8a
gan-ta <gilmat@naver.com>
2025-04-19 15:38:44 +0900

    refactor: CourseEntity AllArgsConstructor 어노테이션 제거

--- src/main/java/nextstep/courses/entity/CourseEntity.java
+++ src/main/java/nextstep/courses/entity/CourseEntity.java
@@ -1,6 +1,5 @@
 package nextstep.courses.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,7 +7,6 @@ import java.time.LocalDateTime;
 
 @Builder
 @Getter
-@AllArgsConstructor
 public class CourseEntity {
     private Long id;
 
