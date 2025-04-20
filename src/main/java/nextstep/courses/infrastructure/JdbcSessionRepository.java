b2d740dc34abcb8526db1234753322e9aa85f76c
panya <kjy2844@kaist.ac.kr>
2025-04-20 18:21:35 +0900

    feat: SessionRepositoryTest 추가

---
+++ src/main/java/nextstep/courses/infrastructure/JdbcSessionRepository.java
@@ -0,0 +1,30 @@
+package nextstep.courses.infrastructure;
+
+import java.util.Optional;
+import nextstep.courses.domain.FreeSession;
+import nextstep.courses.domain.Image;
+import nextstep.courses.domain.Period;
+import nextstep.courses.domain.Session;
+import nextstep.courses.domain.SessionRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class JdbcSessionRepository implements SessionRepository {
+
+    JdbcTemplate jdbcTemplate;
+
+    public JdbcSessionRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Optional<Session> findById(Long id) {
+        Session session = jdbcTemplate.queryForObject("select * from session where id = ?", (rs, rowNum) -> {
+            Image image = jdbcTemplate.queryForObject(
+                "select * from image where id = ?", (image_rs, image_rowNum) -> new Image(image_rs.getString("url"), image_rs.getInt("width"), image_rs.getInt("height"), image_rs.getString("format"), image_rs.getInt("size")), rs.getLong("image_id"));
+            return new FreeSession(image,
+                new Period(rs.getTimestamp("start_at").toLocalDateTime().toLocalDate(), rs.getTimestamp("end_at").toLocalDateTime().toLocalDate()));
+        }, id);
+
+        return Optional.ofNullable(session);
+    }
+}
---
+++ src/test/java/nextstep/courses/infrastructure/SessionRepositoryTest.java
@@ -0,0 +1,38 @@
+package nextstep.courses.infrastructure;
+
+import java.util.Optional;
+import nextstep.courses.domain.Session;
+import nextstep.courses.domain.SessionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JdbcTest
+public class SessionRepositoryTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SessionRepositoryTest.class);
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private SessionRepository sessionRepository;
+
+    @BeforeEach
+    void setUp() {
+        sessionRepository = new JdbcSessionRepository(jdbcTemplate);
+    }
+
+    @Test
+    @DisplayName("id로 조회할 수 있다.")
+    void findById() {
+        Optional<Session> savedSession = sessionRepository.findById(1L);
+        assertThat(savedSession).isPresent();
+        LOGGER.debug("Session: {}", savedSession);
+    }
+}
