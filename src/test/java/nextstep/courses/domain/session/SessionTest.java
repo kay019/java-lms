a24711ada6cfb023d8ea1e0589ea61463cb189ec
luka.redwing(이재석)/kakao <luka.redwing@kakaocorp.com>
2025-04-22 15:24:30 +0900

    refactor SessionTest 에서 MethodSource 사용

--- src/test/java/nextstep/courses/domain/session/SessionTest.java
+++ src/test/java/nextstep/courses/domain/session/SessionTest.java
@@ -4,24 +4,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDate;
 
+import java.util.stream.Stream;
 import nextstep.courses.domain.session.inner.FreeSessionType;
 import nextstep.courses.domain.session.inner.PaidSessionType;
 import nextstep.courses.domain.session.inner.SessionStatus;
 import nextstep.payments.domain.Payment;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class SessionTest {
     public static final Session SESSION1 = new Session(1L, 1L, "free1", new FreeSessionType(), LocalDate.of(2023, 10, 1), LocalDate.of(2023, 10, 31), SessionStatus.OPEN, 0);
     public static final Session SESSION2 = new Session(2L, 1L, "paid1", new PaidSessionType(10, 10000), LocalDate.of(2023, 10, 1), LocalDate.of(2023, 10, 31), SessionStatus.OPEN, 0);
     public static final Session SESSION3 = new Session(3L, 1L, "free2", new FreeSessionType(), LocalDate.of(2023, 10, 1), LocalDate.of(2023, 10, 31), SessionStatus.CLOSED, 0);
 
-    @Test
-    void 수강신청상태(){
-        boolean enroll1 = SESSION1.enroll(new Payment());
-        assertThat(enroll1).isTrue();
+    @ParameterizedTest
+    @MethodSource("수강신청_예상")
+    void 수강신청상태(Session session, boolean expected) {
+        boolean result = session.enroll(new Payment());
+        assertThat(result).isEqualTo(expected);
+    }
 
-        boolean enroll3 = SESSION3.enroll(new Payment());
-        assertThat(enroll3).isFalse();
+    static Stream<Arguments> 수강신청_예상() {
+        return Stream.of(
+                Arguments.of(SESSION1, true),
+                Arguments.of(SESSION3, false)
+        );
     }
 
 }
