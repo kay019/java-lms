cfb1b7ca03ad82c8f85ba293f288ef301d8961e6
jayden(김준기) <jayden.i@kakaocorp.com>
2025-04-23 22:49:30 +0900

    [리팩토링] SessionStatus 를 enum 상태 클래스를 필드로 갖도록 변경한다.

--- src/main/java/nextstep/session/domain/SessionStatus.java
+++ src/main/java/nextstep/session/domain/SessionStatus.java
@@ -1,7 +1,35 @@
 package nextstep.session.domain;
 
-public enum SessionStatus {
-    PREPARING,
-    ENROLLING,
-    CLOSED
+import static nextstep.session.domain.EnrollmentStatus.ENROLLING;
+import static nextstep.session.domain.SessionProgressStatus.CLOSED;
+
+public class SessionStatus {
+    private final SessionProgressStatus progressStatus;
+    private final EnrollmentStatus enrollmentStatus;
+
+    public SessionStatus(SessionProgressStatus progressStatus, EnrollmentStatus enrollmentStatus) {
+        this.progressStatus = progressStatus;
+        this.enrollmentStatus = enrollmentStatus;
+    }
+
+    public SessionStatus(String progressStatus, String enrollmentStatus) {
+        this(SessionProgressStatus.valueOf(progressStatus), EnrollmentStatus.valueOf(enrollmentStatus));
+    }
+
+    public String getProgressStatusName() {
+        return progressStatus.name();
+    }
+
+    public String getEnrollmentStatusName() {
+        return enrollmentStatus.name();
+    }
+
+    public boolean isEnrollmentAvailable() {
+        return enrollmentStatus == ENROLLING
+            && progressStatus != CLOSED;
+    }
+
+    public boolean isEnrollmentUnavailable() {
+        return !isEnrollmentAvailable();
+    }
 }
